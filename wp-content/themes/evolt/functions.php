c4fcc15dd2694c224de444df1fccd689a9e64398
vikascodetrinity <vikas.codetrinity@gmail.com>
2023-02-23 18:57:08 -0500

    some changes in checkout page and shop-list page design

--- wp-content/themes/evolt/functions.php
+++ wp-content/themes/evolt/functions.php
@@ -447,3 +447,25 @@ function custom_empty_cart_message() {
 }
 
 //---------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------my-account wishlist product-list--------------------------------------
+
+
+function function_woosw_wishlist_items_before(  $key, $products  ) {
+    echo  '<thead>
+				<th>
+					<td>Product</td>
+					<td>Product Detail</td>
+					<td></td>
+	 			</th>
+			</thead>';
+}
+add_action( 'woosw_wishlist_items_before', 'function_woosw_wishlist_items_before', 13, 2 );
+
+
+//---------------------------------------------------------------------------------------------------------------------
+
+// add_filter( 'woocommerce_format_sale_price', 'invert_formatted_sale_price', 10, 3 );
+function invert_formatted_sale_price( $price, $regular_price, $sale_price ) {
+    return '<ins>' . ( is_numeric( $sale_price ) ? wc_price( $sale_price ) : $sale_price ) . '</ins> <del>' . ( is_numeric( $regular_price ) ? wc_price( $regular_price ) : $regular_price ) . '</del>';
+}
